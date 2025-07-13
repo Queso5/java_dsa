849985b09e110dc0445a172a19e3bc8f4eb25c46
[tejas parate] <[tejasparate.st@gmail.com]>
2025-07-13 14:39:36 +0530

    day7 ques1

--- src/MissingNUm.java
+++ src/MissingNUm.java
@@ -20,12 +20,37 @@ public class MissingNUm {
 return -1;
     }
 
+    public static int missingNumBetter(int[] arr, int n){
+        boolean[] hash = new boolean[n+1];
+        for(int num:arr){
+            hash[num]=true;
+        }
+
+        for(int i =1; i < n; i++){
+            if(hash[i]== false){
+                    return i;
+            }
+        }
+return  -1;
+    }
+
+
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[] arr = Arrays.stream(sc.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        missingNumBrute(arr);
+        int n = sc.nextInt();
+
+//        missingNumBrute(arr);
+
+        int result = missingNumBetter(arr , n);
+        System.out.println("the missing num using better approach is:"+ result);
+
+
+
+
     }
 }
