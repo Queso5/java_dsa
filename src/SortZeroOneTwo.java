a3cbfad263c6285e2d00b136a416c24b5bd11f57
[tejas parate] <tejasparate.st@gmail.com>
2025-07-26 12:38:18 +0530

    day12 ques2

--- src/SortZeroOneTwo.java
+++ src/SortZeroOneTwo.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.*;
 public class SortZeroOneTwo {
     public static void sort012Better(int[] nums ) {
         int n = nums.length;
@@ -28,13 +29,41 @@ public class SortZeroOneTwo {
         }
     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int[] arr = Arrays.stream(sc.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        sort012Better(arr);
-        System.out.println(args);
+    public static void sort012Optimal(ArrayList<Integer> arr , int n ){
+        int low = 0 ;
+        int mid = 0;
+        int high = n-1;
+        while(mid <= high){
+            if(arr.get(mid) == 0){
+                int temp = arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid , temp);
+                mid++;
+                low++;
+            } else if(arr.get(mid) == 1){
+                mid++;
+            }else {
+                int temp = arr.get(mid);
+                arr.set(mid , arr.get(high));
+                arr.set(high, temp);
+                high--;
+            }
+        }
     }
+
+
+
+    public static void main(String[] args){
+        int n = 6;
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(new Integer[] {0, 2, 1, 2, 0, 1}));
+        sort012Optimal(arr, n);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+
 }
+
