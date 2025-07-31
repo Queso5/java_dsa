befeea44d5babbad035887e706ce06195bb310ab
[tejas parate] <tejasparate.st@gmail.com>
2025-07-31 17:01:18 +0530

    day17 ques 1

--- src/BuySellStock.java
+++ src/BuySellStock.java
@@ -1,16 +1,28 @@
 import java.util.*;
 public class BuySellStock {
-    public static int buySellStockBrute(int[] prices){
+    public static int buySellStockBrute(int[] prices) {
         int n = prices.length;
-        int profit = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i+1; j < n ;j++ ){
-                if(prices[j] > prices[i]){
+        int profit = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] > prices[i]) {
                     profit = Math.max(profit, prices[j] - prices[i]);
                 }
             }
         }
         return profit;
+    }
+
+    public static int buySellStockOptimal(int[] prices) {
+        int maxPro = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxPro = Math.max(maxPro, prices[i] - minPrice);
+        }
+        return maxPro;
+
+
     }
 
     public static void main(String[] args) {
@@ -19,7 +31,7 @@ public class BuySellStock {
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        int result = buySellStockBrute(arr);
+        int result = buySellStockOptimal(arr);
         System.out.println(result);
 
     }
